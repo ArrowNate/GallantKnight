dd9c9d3f5cf73744123470c029bf7a2915b50724
ArrowNate <George.Edmon@triosstudent.com>
2023-10-30 18:41:01 -0400

    Created the Helper Scripts Folder in the Scripts Folder and Created a Tag Manager Script inside of it

---
+++ Gallant Knight/Assets/Scripts/Helper Scripts/TagManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagManager
+{
+    public const string RUN_ANIMATION_PARAM = "Run";
+    public const string JUMP_ANIMATION_PARAM = "Jump";
+}
