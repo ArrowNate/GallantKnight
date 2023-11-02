78033a139498755d0bea9cf6d4253ae40ce646d9
ArrowNate <George.Edmon@triosstudent.com>
2023-11-02 18:40:20 -0400

    Created a Hanging Spike Script and got it where it Falls when it Detects the Player

---
+++ Gallant Knight/Assets/Scripts/Obstacle Script/HangingSpike.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangingSpike : MonoBehaviour
+{
+    [SerializeField] LayerMask collisionLayer;
+
+    private Rigidbody2D sRB;
+    private RaycastHit2D playerCast;
+
+    private bool collidedWitPlayer;
+
+    private void Awake()
+    {
+        sRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        CheckForPlayerCollision();
+    }
+
+    void CheckForPlayerCollision()
+    {
+        if (collidedWitPlayer)
+            return;
+
+        playerCast = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, collisionLayer);
+
+        if (playerCast.collider != null)
+        {
+            collidedWitPlayer = true;
+            sRB.gravityScale = 1f;
+        }
+    }
+}
